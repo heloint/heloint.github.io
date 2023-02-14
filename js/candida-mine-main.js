fd3ac93b52172c22f64f39c6a47236117f9b4822
heloint <majerdaniel93@gmail.com>
2023-02-14 12:50:01 +0100

    To be continued with the Templates menu

---
+++ js/candida-mine-main.js
@@ -0,0 +1,36 @@
+// LOAD HTML SNIPPETS
+$(function () {
+var includes = $("[data-include]");
+$.each(includes, function () {
+  var file = "components/" + $(this).data("include") + ".html";
+  $(this).load(file);
+});
+});
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
