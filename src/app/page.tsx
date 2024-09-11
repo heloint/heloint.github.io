eeec2c7bfcdb0f69d40e5b607be57e9334b43d47
heloint <majerdaniel93@gmail.com>
2024-09-11 16:59:14 +0200

    tbc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+import path from 'path';
+import { remark } from 'remark';
+import html from 'remark-html';
 
-export default function Home() {
+import * as fs from 'fs';
+import matter from 'gray-matter';
+
+export default async function Home() {
+  const markdownPath = path.join(process.cwd(), "_markdown_contents", "posts", "1.md")
+  const fileContents = fs.readFileSync(markdownPath, 'utf8');
+  const matterResult = matter(fileContents)
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
   return (
-    <div className="">
+    <div className="" dangerouslySetInnerHTML={{__html: contentHtml}}>
     </div>
   );
 }
